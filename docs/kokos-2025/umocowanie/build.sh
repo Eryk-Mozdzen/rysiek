23ec709413a0f6f7a585e408960da9609c45efd9
Eryk Możdżeń <ermozdzen@gmail.com>
2025-04-17 16:52:21 +0200

    umocowanie wypełnione bez podpisów

---
+++ docs/kokos-2025/umocowanie/build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+pdflatex -interaction=nonstopmode -shell-escape umocowanie > /dev/null
+pdflatex -interaction=nonstopmode -shell-escape umocowanie > /dev/null
---
+++ docs/kokos-2025/umocowanie/open.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+nohup firefox -new-tab umocowanie.pdf > /dev/null 2>&1 &
